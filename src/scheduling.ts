6862b0061ab059909a50646c44baadb0b4f669d5
Gableb <gableb@hotmail.fr>
2024-09-23 13:11:18 +0000

    timezone-based scheduling

--- src/scheduling.ts
+++ src/scheduling.ts
@@ -1,20 +1,30 @@
-
 import { join as pathJoin } from "node:path";
 import { gracefulShutdown, Job, scheduleJob } from "node-schedule";
 import { playSound } from "./sound-system";
 import { Channel, Guild } from "discord.js";
-import { SoundConfig } from "./config";
+import { config, SoundConfig } from "./config";
 
 export const jobs: Job[] = [];
 
-export function scheduleSound(guild: Guild, channel:Channel, sound:SoundConfig, ){
+export function scheduleSound(
+  guild: Guild,
+  channel: Channel,
+  sound: SoundConfig
+) {
   jobs.push(
-    scheduleJob(guild.id + ":" + sound.name, sound.schedule, () => {
-      playSound(guild, channel.id, pathJoin("sounds", sound.name));
-    })
+    scheduleJob(
+      guild.id + ":" + sound.name,
+      {
+        rule: sound.schedule,
+        tz: config.timezone,
+      },
+      () => {
+        playSound(guild, channel.id, pathJoin("sounds", sound.name));
+      }
+    )
   );
 }
 
-export async function cancelSchedule(){
+export async function cancelSchedule() {
   await gracefulShutdown(); //Cancel all jobs
 }
