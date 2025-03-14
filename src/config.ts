0df6fcf65c0d675ff7ce045ab94928691c6ce0f6
Gableb <gabriel.lebis@free.fr>
2025-03-14 14:57:31 +0000

    add config persistence

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,17 @@
 import { join as pathJoin } from "node:path";
 import { setupGuilds } from ".";
+import fspromises from "node:fs/promises";
+
+const CONFIG_JSON_PATH = pathJoin(
+  process.cwd(),
+  "config",
+  "config.json"
+);
+const SOUNDS_JSON_PATH = pathJoin(
+  process.cwd(),
+  "config",
+  "sounds.json"
+);
 
 export interface SoundConfig {
   name: string;
@@ -16,20 +28,12 @@ interface Sounds {
   [soundName: string]: { schedule: string };
 }
 
-export const config = require(pathJoin(
-  process.cwd(),
-  "config",
-  "config.json"
-)) as Config;
-
-export const sounds = require(pathJoin(
-  process.cwd(),
-  "config",
-  "sounds.json"
-)) as Sounds;
+export const config = require(CONFIG_JSON_PATH) as Config;
+export const sounds = require(SOUNDS_JSON_PATH) as Sounds;
 
 export function setTimezone(newTimezone: string) {
   config.timezone = newTimezone;
+  saveConfigJson();
   setupGuilds();
 }
 
@@ -47,6 +51,19 @@ export function setSoundSchedule(
     return false;
   }
   sounds[soundName] = { schedule: newSchedule };
+  saveSoundsJson();
   setupGuilds();
   return true;
+}
+
+async function saveSoundsJson(){
+  console.log(`Saving sounds.json...`);
+  await fspromises.writeFile(SOUNDS_JSON_PATH, JSON.stringify(sounds, null, 2));
+  console.log(`Saved sounds.json`)
+}
+
+async function saveConfigJson(){
+  console.log(`Saving config.json`);
+  await fspromises.writeFile(CONFIG_JSON_PATH, JSON.stringify(config, null, 2));
+  console.log(`Saved config.json`)
 }
